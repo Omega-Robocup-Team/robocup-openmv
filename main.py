6b372dc00a74830ccbfb2cb6567984a2b0ecde74
Pasha Matveev <pasha.matveev.1287@mail.ru>
2024-08-25 19:11:29 +0500

    add searching roi

--- main.py
+++ main.py
@@ -2,18 +2,111 @@ import pyb
 import ustruct
 import time
 import sensor
-import math
 import image
+import math
 
 debug_mode = True
+robot_num = 0
 
 ball_threshold = [(26, 75, 5, 37, 21, 46), (30, 50, 5, 36, 15, 35)]
 yellow_threshold = [(58, 76, -27, -2, 25, 56)]
 blue_threshold = [(30, 50, -15, 15, -25, -10)]
 
-center = (354, 211)
-#center = (284, 194)
-radius = 260
+center = (284, 194) if robot_num else (354, 211)
+radius = 260 if robot_num else 260
+
+
+class TrackedObject:
+    def __init__(self, obj_type: bool, threshold: list, search_margin: int, min_pixels: int, max_pixels: int) -> None:
+        self.type = obj_type  # 0:ball | 1:goal
+        self.threshold = threshold
+        self.margin = search_margin
+        self.min_pixels = min_pixels
+        self.max_pixels = max_pixels
+        self.roi = None
+        self.new_value = False
+        self.best_match = None
+        self.angle = 0
+        self.dist = 0
+        self.blobs = []
+
+    def get_roi(self, margin) -> tuple:
+        if self.best_match is not None:
+            self.roi = []
+            self.roi.append(
+                max(0, center[0] - radius, self.best_match.x() - self.best_match.w()/2 - margin))
+            self.roi.append(
+                max(0, center[1] - radius, self.best_match.y() - self.best_match.h()/2 - margin))
+            self.roi.append(
+                min(margin * 2 + self.best_match.w(), sensor.width() - self.roi[0], center[0] + radius - self.roi[0]))
+            self.roi.append(
+                min(margin * 2 + self.best_match.h(), sensor.height() - self.roi[1], center[1] + radius - self.roi[1]))
+        else:
+            self.roi = []
+            self.roi.append(max(0, center[0] - radius))
+            self.roi.append(max(0, center[1] - radius))
+            self.roi.append(min(center[0] + radius, sensor.width()))
+            self.roi.append(min(center[1] + radius, sensor.height()))
+        return self.roi
+
+    def filter_blob(self, blob) -> bool:
+        res = 1
+        res *= self.min_pixels <= blob.pixels() <= self.max_pixels
+        res *= math.sqrt((blob.cx() - center[0]) **
+                         2 + (blob.cy() - center[1]) ** 2) <= radius
+        return res
+
+    def evaluate_blob(self, blob) -> float:
+        res = blob.pixels()
+        res *= blob.roundness() if self.type == 0 else 1
+        return res
+
+    def get_vector(self, blob) -> None:
+        self.angle = int(math.atan2(
+            blob.y() - center[1], blob.x() - center[0]) / math.pi * 180)
+        self.dist = int(math.sqrt(
+            (blob.cx() - center[0]) ** 2 + (blob.cy() - center[1]) ** 2) / radius * 100)
+
+    def find(self, img) -> None:
+        self.get_roi(self.margin)
+        self.blobs = img.find_blobs(
+            self.threshold,
+            threshold_cb=self.filter_blob,
+            roi=self.roi,
+            merge=self.type,
+            margin=20 if self.type else 0,
+            x_stride=10 if self.type else 1,
+            y_stride=10 if self.type else 1,
+        )
+        if len(self.blobs) > 0:
+            self.best_match = max(self.blobs, key=self.evaluate_blob)
+            self.get_vector(self.best_match)
+            self.new_value = True
+        else:
+            self.best_match = None
+
+    def draw_roi(self, img) -> None:
+        if self.roi is not None:
+            img.draw_rectangle(self.roi, color=(255, 255, 255), thickness=1)
+
+    def draw_vector(self, img) -> None:
+        if self.best_match is not None:
+            img.draw_line((self.best_match.cx(), self.best_match.cy(
+            ), center[0], center[1]), color=(255, 255, 255), thickness=1)
+
+    def draw_blob(self, img, blob, color) -> None:
+        if self.type == 0:
+            img.draw_ellipse(blob.enclosed_ellipse(), color=color, thickness=1)
+        else:
+            img.draw_rectangle(blob.rect(), color=color, thickness=1)
+
+    def draw(self, img) -> None:
+        self.draw_roi(img)
+        self.draw_vector(img)
+        for blob in self.blobs:
+            self.draw_blob(img, blob, (0, 255, 0)
+                           if blob is self.best_match else (255, 255, 255))
+
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -21,11 +114,22 @@ sensor.set_framesize(sensor.VGA)
 sensor.skip_frames(time=2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
-clock = time.clock()
 
-text = '0 000 000 0 000 000 0 000 000'
-data = ustruct.pack("<bi%ds" % len(text), 85, len(text), text)
+clock = time.clock()
 spi = pyb.SPI(2, pyb.SPI.SLAVE, polarity=0, phase=0)
+
+ball = TrackedObject(0, ball_threshold, 50, 5, 50)
+yellow_goal = TrackedObject(1, yellow_threshold, 50, 20, 300)
+blue_goal = TrackedObject(1, blue_threshold, 50, 20, 300)
+
+data = None
+
+
+def generate_data():
+    global data
+    data = ustruct.pack('<bbhhbhhbhh', 85, ball.new_value, ball.angle, ball.dist, yellow_goal.new_value,
+                        yellow_goal.angle, yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist)
+
 
 def nss_callback(line):
     global spi, data
@@ -34,115 +138,26 @@ def nss_callback(line):
     except OSError as err:
         pass
 
-pyb.ExtInt(pyb.Pin("P3"), pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_UP, nss_callback)
 
-class Object:
-    def __init__(self, blob, obj_type) -> None:
-        self.blob = blob
-        self.angle = 0
-        self.dist = 0
-        self.value = 0
-        self.obj_type = obj_type
-
-    def is_in_mirror(self):
-        return math.sqrt((self.blob.cx() - center[0]) ** 2 + (self.blob.cy() - center[1]) ** 2) < radius
-
-    def evaluate(self):
-        self.value = 1
-        if self.obj_type == 0:
-            self.value *= self.blob.pixels()
-            self.value *= self.blob.roundness()
-            self.value *= self.blob.pixels() < 2000
-            self.value *= self.is_in_mirror()
-        else:
-            self.value *= self.blob.pixels()
-            self.value *= self.is_in_mirror()
-        return self.value
-
-    def calc_vector(self):
-        self.angle = int(math.atan2(self.blob.cx() - center[0], self.blob.cy() - center[1]) / math.pi * 180 - 90 + 360) % 360
-        self.dist = int(math.sqrt((self.blob.cx() - center[0]) ** 2 + (self.blob.cy() - center[1]) ** 2) / radius * 100)
-
-    def draw(self, img, i, color):
-        if self.obj_type == 0:
-            img.draw_ellipse(self.blob.enclosed_ellipse(), color=color)
-            img.draw_string(self.blob.cx(), self.blob.cy(), str(i), color=color, scale=2)
-        else:
-            img.draw_rectangle(self.blob.rect(), color=color)
-            img.draw_string(self.blob.cx(), self.blob.cy(), str(i), color=color, scale=2)
-
-    def draw_vector(self, img):
-        img.draw_line(center[0], center[1], self.blob.cx(), self.blob.cy(), color=(0, 255, 0))
-
-
-
-class ObjectArray:
-    def __init__(self, threshold, obj_type) -> None:
-        self.threshold = threshold
-        self.obj_type = obj_type # 0 - ball, 1 - yellow goal, 2 - blue goal
-        self.candidates = []
-        self.match = None
-
-    def generate(self, blob_list):
-        self.candidates = []
-        for blob in blob_list:
-            self.candidates.append(Object(blob, self.obj_type))
-            self.candidates[-1].evaluate()
-
-    def find(self, img):
-        if (self.obj_type == 0):
-            self.generate(img.find_blobs(ball_threshold, pixels_threshold=20, area_threshold=20))
-        else:
-            self.generate(img.find_blobs(self.threshold, pixels_threshold=100, area_threshold=100, merge=True, margin=20))
-
-        self.candidates = sorted(self.candidates, key=lambda obj: obj.value, reverse=True)
-
-        self.match = None
-        if len(self.candidates):
-            if self.candidates[0].value > 0:
-                self.match = self.candidates[0]
-
-    def draw(self, img):
-        for i in range(len(self.candidates)):
-            if self.candidates[i].value > 0:
-                if i == 0:
-                    self.candidates[i].draw(img, i, color=(0, 255, 0))
-                    self.candidates[i].draw_vector(img)
-                else:
-                    self.candidates[i].draw(img, i, color=(255, 255, 255))
-            else:
-                self.candidates[i].draw(img, i, color=(255, 0, 0))
-
-    def trim(self, text):
-        return (3 - len(str(text))) * '0' + str(text)
-
-    def text(self):
-        if self.match != None:
-            self.match.calc_vector()
-            return '1 ' + self.trim(self.match.angle) + ' ' + self.trim(self.match.dist)
-        else:
-            return '0 000 000'
-
-ball_array = ObjectArray(ball_threshold, 0)
-yellow_array = ObjectArray(yellow_threshold, 1)
-blue_array = ObjectArray(blue_threshold, 2)
+generate_data()
+pyb.ExtInt(pyb.Pin("P3"), pyb.ExtInt.IRQ_FALLING,
+           pyb.Pin.PULL_UP, nss_callback)
 
 while True:
     clock.tick()
     img = sensor.snapshot()
 
-    ball_array.find(img)
-    yellow_array.find(img)
-    blue_array.find(img)
+    ball.find(img)
+    yellow_goal.find(img)
+    blue_goal.find(img)
 
-    text = ball_array.text() + ' ' + yellow_array.text() + ' ' + blue_array.text()
-    data = ustruct.pack("<bi%ds" % len(text), 85, len(text), text)
+    generate_data()
 
     if debug_mode:
-        ball_array.draw(img)
-        yellow_array.draw(img)
-        blue_array.draw(img)
+        yellow_goal.draw()
+        blue_goal.draw()
+        ball.draw()
         img.draw_cross(center)
         img.draw_circle(center[0], center[1], radius)
 
-    print(text, 'fps:', clock.fps())
+    print(ball.new_value, , 'fps:', clock.fps())
