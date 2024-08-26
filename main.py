1c0502ee1f8a1e40db2e78d1a4dd09059bf88044
Pasha Matveev <pasha.matveev.1287@mail.ru>
2024-08-27 00:22:59 +0300

    fixed roi

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import image
 import math
 
 debug_mode = True
-robot_num = 0
+robot_num = 1
 
-ball_threshold = [(26, 75, 5, 37, 21, 46), (30, 50, 5, 36, 15, 35)]
-yellow_threshold = [(58, 76, -27, -2, 25, 56)]
+ball_threshold = [(40, 59, 5, 30, 10, 39)]
+yellow_threshold = [(40, 70, -20, 10, 20, 40)]
 blue_threshold = [(30, 50, -15, 15, -25, -10)]
 
-center = (284, 194) if robot_num else (354, 211)
-radius = 260 if robot_num else 260
+center = (358, 245) if robot_num else (354, 211)
+radius = 340 if robot_num else 260
 
 
 class TrackedObject:
@@ -29,24 +29,30 @@ class TrackedObject:
         self.angle = 0
         self.dist = 0
         self.blobs = []
+        self.refresh_time = 5
+        self.time_buf = time.time()
 
     def get_roi(self, margin) -> tuple:
+        if time.time() - self.time_buf > self.refresh_time:
+            self.best_match = None
+            self.time_buf = time.time()
         if self.best_match is not None:
             self.roi = []
             self.roi.append(
-                max(0, center[0] - radius, self.best_match.x() - self.best_match.w()/2 - margin))
+                max(0, center[0] - radius, self.best_match.x() - margin))
             self.roi.append(
-                max(0, center[1] - radius, self.best_match.y() - self.best_match.h()/2 - margin))
+                max(0, center[1] - radius, self.best_match.y() - margin))
             self.roi.append(
-                min(margin * 2 + self.best_match.w(), sensor.width() - self.roi[0], center[0] + radius - self.roi[0]))
+                min(self.best_match.x() + self.best_match.w() + margin, center[0] + radius, sensor.width()) - self.roi[0])
             self.roi.append(
-                min(margin * 2 + self.best_match.h(), sensor.height() - self.roi[1], center[1] + radius - self.roi[1]))
+                min(self.best_match.y() + self.best_match.h() + margin, center[1] + radius, sensor.height()) - self.roi[1])
         else:
             self.roi = []
             self.roi.append(max(0, center[0] - radius))
             self.roi.append(max(0, center[1] - radius))
-            self.roi.append(min(center[0] + radius, sensor.width()))
-            self.roi.append(min(center[1] + radius, sensor.height()))
+            self.roi.append(min(center[0] + radius, sensor.width()) - self.roi[0])
+            self.roi.append(min(center[1] + radius, sensor.height()) - self.roi[1])
+        self.roi = list(map(int, self.roi))
         return self.roi
 
     def filter_blob(self, blob) -> bool:
@@ -74,9 +80,9 @@ class TrackedObject:
             threshold_cb=self.filter_blob,
             roi=self.roi,
             merge=self.type,
-            margin=20 if self.type else 0,
-            x_stride=10 if self.type else 1,
-            y_stride=10 if self.type else 1,
+            margin=25 if self.type else 0,
+            x_stride=10 if self.type else 5,
+            y_stride=10 if self.type else 5
         )
         if len(self.blobs) > 0:
             self.best_match = max(self.blobs, key=self.evaluate_blob)
@@ -87,7 +93,7 @@ class TrackedObject:
 
     def draw_roi(self, img) -> None:
         if self.roi is not None:
-            img.draw_rectangle(self.roi, color=(255, 255, 255), thickness=1)
+            img.draw_rectangle(self.roi, color=(255, 0, 0), thickness=1)
 
     def draw_vector(self, img) -> None:
         if self.best_match is not None:
@@ -107,10 +113,9 @@ class TrackedObject:
             self.draw_blob(img, blob, (0, 255, 0)
                            if blob is self.best_match else (255, 255, 255))
 
-
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.VGA)
+sensor.set_framesize(sensor.SVGA)
 sensor.skip_frames(time=2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
@@ -118,26 +123,26 @@ sensor.set_auto_whitebal(False)
 clock = time.clock()
 spi = pyb.SPI(2, pyb.SPI.SLAVE, polarity=0, phase=0)
 
-ball = TrackedObject(0, ball_threshold, 50, 5, 50)
-yellow_goal = TrackedObject(1, yellow_threshold, 50, 20, 300)
-blue_goal = TrackedObject(1, blue_threshold, 50, 20, 300)
+ball = TrackedObject(0, ball_threshold, 50, 100, 3000)
+yellow_goal = TrackedObject(1, yellow_threshold, 50, 500, 20000)
+blue_goal = TrackedObject(1, blue_threshold, 50, 5000, 20000)
 
 data = None
 
 
 def generate_data():
-    global data, ball, yellow_goal, blue_goal
+    global data
     data = ustruct.pack('<bbhhbhhbhh', 85, ball.new_value, ball.angle, ball.dist, yellow_goal.new_value,
                         yellow_goal.angle, yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist)
-    ball.new_value = 0
-    yellow_goal.new_value = 0
-    blue_goal.new_value = 0
 
 
 def nss_callback(line):
-    global spi, data
+    global spi, data, ball, yellow_goal, blue_goal
     try:
         spi.send(data, timeout=1000)
+        ball.new_value = 0
+        yellow_goal.new_value = 0
+        blue_goal.new_value = 0
     except OSError as err:
         pass
 
@@ -151,15 +156,15 @@ while True:
     img = sensor.snapshot()
 
     ball.find(img)
-    yellow_goal.find(img)
-    blue_goal.find(img)
+#    yellow_goal.find(img)
+#    blue_goal.find(img)
 
     generate_data()
 
     if debug_mode:
-        yellow_goal.draw()
-        blue_goal.draw()
-        ball.draw()
+#        yellow_goal.draw(img)
+#        blue_goal.draw(img)
+        ball.draw(img)
         img.draw_cross(center)
         img.draw_circle(center[0], center[1], radius)
 
