1fddd02c6486fbe99ce81d99896c82aca6a56ee5
Pasha Matveev <pasha.matveev.1287@mail.ru>
2024-09-14 03:21:49 +0700

    minor changes

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import sensor
 import image
 import math
 
-debug_mode = True
+debug_mode = 1
 robot_num = 1
 
-ball_threshold = [(40, 59, 5, 30, 10, 39)]
-yellow_threshold = [(40, 70, -20, 10, 20, 40)]
-blue_threshold = [(30, 50, -15, 15, -25, -10)]
+#ball_threshold = [(49, 75, 14, 51, -1, 49)]
+ball_threshold = [(20, 45, 0, 30, 4, 34)]
+#yellow_threshold = [(73, 87, -6, 51, 28, 56)]
+yellow_threshold = [(35, 39, -10, 7, 12, 34)]
+#blue_threshold = [(41, 56, -34, 51, -45, -19)]
+blue_threshold = [(25, 31, -14, 5, -17, -1)]
 
-center = (358, 245) if robot_num else (354, 211)
-radius = 340 if robot_num else 260
+center = (365, 247) if robot_num else (365, 247)
+radius = 340 if robot_num else 340
+inner_radius = 75
+
+fps = 0
 
 
 class TrackedObject:
@@ -25,6 +31,7 @@ class TrackedObject:
         self.max_pixels = max_pixels
         self.roi = None
         self.new_value = 0
+        self.value_id = 0
         self.best_match = None
         self.angle = 0
         self.dist = 0
@@ -58,8 +65,9 @@ class TrackedObject:
     def filter_blob(self, blob) -> bool:
         res = 1
         res *= self.min_pixels <= blob.pixels() <= self.max_pixels
-        res *= math.sqrt((blob.cx() - center[0]) **
-                         2 + (blob.cy() - center[1]) ** 2) <= radius
+        res *= math.sqrt((blob.cx() - center[0]) ** 2 + (blob.cy() - center[1]) ** 2) <= radius
+        res *= math.sqrt((blob.cx() - center[0]) ** 2 + (blob.cy() - center[1]) ** 2) >= inner_radius
+
         return res
 
     def evaluate_blob(self, blob) -> float:
@@ -68,10 +76,8 @@ class TrackedObject:
         return res
 
     def get_vector(self, blob) -> None:
-        self.angle = int(math.atan2(
-            blob.y() - center[1], blob.x() - center[0]) / math.pi * 180)
-        self.dist = int(math.sqrt(
-            (blob.cx() - center[0]) ** 2 + (blob.cy() - center[1]) ** 2) / radius * 100)
+        self.angle = int(math.atan2(blob.cx() - center[0], blob.cy() - center[1]) / math.pi * 180 - 90 + 360) % 360
+        self.dist = int(math.sqrt((blob.cx() - center[0]) ** 2 + (blob.cy() - center[1]) ** 2) / radius * 100)
 
     def find(self, img) -> None:
         self.get_roi(self.margin)
@@ -87,9 +93,11 @@ class TrackedObject:
         if len(self.blobs) > 0:
             self.best_match = max(self.blobs, key=self.evaluate_blob)
             self.get_vector(self.best_match)
-            self.new_value = 1
+            self.value_id = self.value_id % 100 + 1
+            self.new_value = self.value_id
         else:
             self.best_match = None
+            self.new_value = 0
 
     def draw_roi(self, img) -> None:
         if self.roi is not None:
@@ -120,32 +128,30 @@ sensor.skip_frames(time=2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
 
+sensor.set_brightness(3)
+
 clock = time.clock()
 spi = pyb.SPI(2, pyb.SPI.SLAVE, polarity=0, phase=0)
 
-ball = TrackedObject(0, ball_threshold, 50, 100, 3000)
-yellow_goal = TrackedObject(1, yellow_threshold, 50, 500, 20000)
-blue_goal = TrackedObject(1, blue_threshold, 50, 5000, 20000)
+ball = TrackedObject(0, ball_threshold, 50, 50, 8000)
+yellow_goal = TrackedObject(1, yellow_threshold, 50, 0, 20000)
+blue_goal = TrackedObject(1, blue_threshold, 50, 0, 20000)
 
 data = None
 
 
 def generate_data():
     global data
-    data = ustruct.pack('<bbhhbhhbhh', 85, ball.new_value, ball.angle, ball.dist, yellow_goal.new_value,
-                        yellow_goal.angle, yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist)
+    data = ustruct.pack('<bbhhbhhbhhb', 85, ball.new_value, ball.angle, ball.dist, yellow_goal.new_value,
+                        yellow_goal.angle, yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist, int(fps))
 
 
 def nss_callback(line):
     global spi, data, ball, yellow_goal, blue_goal
     try:
         spi.send(data, timeout=1000)
-        ball.new_value = 0
-        yellow_goal.new_value = 0
-        blue_goal.new_value = 0
     except OSError as err:
         pass
-
 
 generate_data()
 pyb.ExtInt(pyb.Pin("P3"), pyb.ExtInt.IRQ_FALLING,
@@ -156,17 +162,19 @@ while True:
     img = sensor.snapshot()
 
     ball.find(img)
-#    yellow_goal.find(img)
-#    blue_goal.find(img)
+    yellow_goal.find(img)
+    blue_goal.find(img)
 
     generate_data()
 
     if debug_mode:
-#        yellow_goal.draw(img)
-#        blue_goal.draw(img)
+        yellow_goal.draw(img)
+        blue_goal.draw(img)
         ball.draw(img)
         img.draw_cross(center)
         img.draw_circle(center[0], center[1], radius)
+        img.draw_circle(center[0], center[1], inner_radius)
 
+    fps = clock.fps()
     print(ball.new_value, ball.angle, ball.dist, yellow_goal.new_value, yellow_goal.angle,
-          yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist, 'fps:', clock.fps())
+          yellow_goal.dist, blue_goal.new_value, blue_goal.angle, blue_goal.dist, 'fps:', fps)
